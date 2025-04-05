27aa7b73a851fa668fc1dd4f09bd56e9bd2d4484
HammerheadShark666 <103519731+HammerheadShark666@users.noreply.github.com>
2025-04-05 15:31:41 +0100

    Delete user gallery photo functionality

---
+++ jwm-photography-api.MediatR/UserGallery/DeleteUserGalleryPhoto/DeleteUserGalleryPhotoMapper.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+
+namespace jwm_photography_api.MediatR.UserGallery.DeleteUserGalleryPhoto;
+
+public class DeleteUserGalleryPhotoMapper : Profile
+{
+    public DeleteUserGalleryPhotoMapper()
+    {
+        //base.CreateMap<DeleteUserGalleryRequest, Domain.UserGallery>(); 
+    }
+}
---
+++ jwm-photography-api.MediatR/UserGallery/DeleteUserGalleryPhoto/DeleteUserGalleryPhotoValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using jwm_photography_api.Data.Repository.Interfaces;
+
+namespace jwm_photography_api.MediatR.UserGallery.DeleteUserGalleryPhoto;
+
+public class DeleteUserGalleryPhotoValidator : AbstractValidator<DeleteUserGalleryPhotoRequest>
+{
+    private readonly IUserGalleryPhotoRepository _userGalleryPhotoRepository;
+
+    public DeleteUserGalleryPhotoValidator(IUserGalleryPhotoRepository userGalleryPhotoRepository)
+    {
+        _userGalleryPhotoRepository = userGalleryPhotoRepository;
+
+        RuleFor(DeleteUserGalleryPhotoRequest => DeleteUserGalleryPhotoRequest).MustAsync(async (DeleteUserGalleryPhotoRequest, cancellation) =>
+        {
+            return await UserGalleryPhotoExists(DeleteUserGalleryPhotoRequest.AccountId, DeleteUserGalleryPhotoRequest.UserGalleryId, DeleteUserGalleryPhotoRequest.PhotoId);
+        }).WithMessage("User gallery photo not found.");
+    }
+
+    protected async Task<bool> UserGalleryPhotoExists(Guid accountId, long userGalleryId, long photoId)
+    {
+        return await _userGalleryPhotoRepository.ExistsAsync(accountId, userGalleryId, photoId);
+    }
+}
